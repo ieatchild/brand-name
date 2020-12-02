fd097f8ddf0f3e33280e5555a1cc55bc31132cca
ieatchild <64801684+ieatchild@users.noreply.github.com>
2020-12-02 20:50:53 +0100

    Homepage finished
    Just finished the homepage, wih lazyness not gonna lie. The background image at the top might change with the time (I find it too bright). Anyways, hope you'll enjoy my work here and that I inspired some people.

--- Brand name/src/app.js
+++ Brand name/src/app.js
@@ -11,3 +11,27 @@ window.addEventListener('scroll', function() {
 
 var transformWrapper = document.getElementById('transform-wrapper');
 setTimeout(function() {transformWrapper.style.transform = 'translateY(-100%)'}, 750);
+
+document.getElementById('container').style.display = 'none';
+
+//mobile version
+
+var mobHeader = document.getElementById('mob-hed')
+
+window.addEventListener('scroll', function() {
+        var scroll = scrollY;
+        if (scroll >= 64) {
+                mobHeader.classList.add('small');
+        } else {
+                mobHeader.classList.remove('small');
+        };
+    })
+
+
+var burger = document.getElementById('header-brgr');
+var mobNavBar = document.getElementById('m-sub-t');
+
+burger.addEventListener('click', function() {
+        burger.classList.toggle('open');
+        mobNavBar.classList.toggle('down');
+})
